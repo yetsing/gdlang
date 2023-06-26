e1a395bc881a3744b77b62590b7beb969d1b16bb
yeqing <heopro@163.com>
2023-06-26 22:18:03 +0800

    add repl

---
+++ repl/repl.go
@@ -0,0 +1,48 @@
+package repl
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"weilang/lexer"
+	"weilang/token"
+)
+
+const (
+	START_PROMPT    = ">> "
+	CONTINUE_PROMPT = "..."
+)
+
+var PROMPT = START_PROMPT
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	var buffer bytes.Buffer
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		buffer.WriteString(line)
+		input := buffer.String()
+		buffer.Reset()
+
+		l := lexer.New(input)
+		for {
+			tok := l.NextToken()
+			if tok.TypeIs(token.EOF) {
+				break
+			}
+			if tok.TypeIs(token.ILLEGAL) {
+				fmt.Println("illegal token", tok)
+				break
+			}
+			fmt.Println(tok)
+		}
+	}
+}
